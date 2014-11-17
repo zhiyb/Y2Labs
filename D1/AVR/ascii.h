a3fecc4b2016184f4e8f68d28df446dea3305b71
zhiyb <yz39g13@soton.ac.uk>
2014-11-17 03:57:41 +0000

    D1 AVR 8bit multiplier tester initial

---
+++ D1/AVR/ascii.h
@@ -0,0 +1,8 @@
+#ifndef ASCII_H
+#define ASCII_H
+
+#include <avr/pgmspace.h>
+
+extern const unsigned char ascii[96][8] PROGMEM;
+
+#endif
---
+++ D1/AVR/main.cpp
@@ -0,0 +1,110 @@
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include "tft.h"
+#include "conv.h"
+
+// PORTB
+#define FUNC0	_BV(0)
+#define FUNC1	_BV(1)
+#define READY	_BV(4)
+#define START	_BV(5)
+#define OE	_BV(7)
+
+// PORTD as data
+
+#define MAX	0x00
+//#define MAX	0x0F
+
+using namespace conv;
+
+void init(void)
+{
+	DDRD = 0x00;
+	PORTD = 0x00;
+	DDRB = START | OE | FUNC0 | FUNC1;
+	PORTB = 0x00;
+	//DDRB |= 0x80;			// LED
+	//PORTB |= 0x80;
+	tft.init();
+	tft /= tft.Portrait;
+	tft.setBackground(0x0000);
+	tft.setForeground(0x667F);
+	tft.clean();
+	stdout = tftout();
+	tft++;
+}
+
+void test(uint8_t a, uint8_t b)
+{
+	uint8_t ra = 0, rb = 0;
+	uint16_t res = (uint16_t)a * (uint16_t)b;
+
+	// Write A
+	DDRD = 0x00;
+	PORTB = 0x00;		// Write A
+	PORTD = a;
+	DDRD = 0xFF;
+	_delay_us(1);
+
+	// Write B
+	PORTB = 0x01;		// Write B
+	PORTD = b;
+	_delay_us(1);
+	PORTB = 0x02;		// Read L
+	DDRD = 0x00;
+	PORTD = 0xFF;
+
+	// Start
+	PORTB |= START;
+	while (PINB & READY);
+	PORTB &= ~START;
+	while (!(PINB & READY));
+
+	// Read L
+	PORTB = 0x02 | OE;	// Read L
+	_delay_us(1);
+	ra = PIND;
+
+	// Read H
+	PORTB = 0x03 | OE;	// Read H
+	_delay_us(1);
+	rb = PIND;
+	PORTB = 0x00;
+
+	// Check
+	uint16_t recv = ((uint16_t)rb << 8) | (uint16_t)ra;
+	if (res != recv) {
+		tft.setForeground(c32to16(0xFF0000));
+		printf("%04X/%04X ", res, recv);
+		tft.setForeground(c32to16(0x00FF00));
+	} else if (b == 0 /*&& (a % 1 == 0)*/)
+			printf("%02X%02X ", (uint16_t)a, (uint16_t)b);
+			//printf("%02X%02X/%04X ", (uint16_t)a, (uint16_t)b, res);
+}
+
+int main(void)
+{
+	init();
+
+	tft.clean();
+	tft *= 2;
+	puts("*** Multiplier ***");
+	tft *= 1;
+	tft.setForeground(c32to16(0x00FF00));
+
+	uint8_t a = 0;
+	do {
+		uint8_t b = 0;
+		do
+			test(a, b);
+		while (++b != (uint8_t)MAX);
+	} while (++a != (uint8_t)MAX);
+
+	tft.setForeground(c32to16(0x0000FF));
+	tft *= 2;
+	printf("Done");
+	while (1);
+
+	return 1;
+}
