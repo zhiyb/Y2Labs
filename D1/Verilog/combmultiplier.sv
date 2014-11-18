e19b8134e6a08d25a28788807a008f50240f924c
zhiyb <yz39g13@soton.ac.uk>
2014-11-18 00:01:32 +0000

    D1 Combinational multiplier fixed

--- D1/Verilog/combmultiplier.sv
+++ D1/Verilog/combmultiplier.sv
@@ -9,19 +9,19 @@ genvar x, y;
 generate
 	for (y = 0; y < n; y++)
 		for (x = 0; x < n; x++)
-			fulladder a0 (.A(a[y][x]), .B(B[y]), .Cin(y == 0 ? '0 : c[y - 1][x]), .S(s[y][x]), .Cout(c[y][x]));
+			fulladder a0 (.A(a[y][x]), .B(A[x] & B[y]), .Cin(x == 0 ? '0 : c[y][x - 1]), .S(s[y][x]), .Cout(c[y][x]));
 endgenerate
 
 // Connections
 genvar i, j;
 generate
-	for (j = 1; j < n; j++)
-		assign a[j][3] = 'b0;
-	for (i = 0; i < n; i++)
-		assign a[0][i] = A[i];
-	for (j = 1; j < n; j++)
-		for (i = 0; i < n - 1; i++)
-			assign a[j][i] = s[j - 1][i + 1];
+	for (x = 0; x < n; x++)
+		assign a[0][x] = 'b0;
+	for (y = 1; y < n; y++)
+		assign a[y][n - 1] = c[y - 1][n - 1];
+	for (y = 1; y < n; y++)
+		for (x = 0; x < n - 1; x++)
+			assign a[y][x] = s[y - 1][x + 1];
 	assign Q[n * 2 - 1] = c[n - 1][n - 1];
 	for (i = 0; i < n; i++)
 		assign Q[i] = s[i][0];
