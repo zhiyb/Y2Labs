2ffc0cf6679e53a7dc4e19bda95e63bcdab58cbf
zhiyb <yz39g13@soton.ac.uk>
2014-11-19 18:38:23 +0000

    D1 MachXO2 pin assignments

--- D1/Verilog/MachXO2/multiplier.sv
+++ D1/Verilog/MachXO2/multiplier.sv
@@ -1,31 +1,41 @@
-// D1 - model solution
-// encapsulating module
-/*
-   author: tjk
-   last revision: 17 Oct' 13
-*/
-
-module multiplier(input logic start, output logic[7:0] AQ);
-
-logic osc_clk, clock, C, ready, reset, shift, add_shift;
-logic [3:0] M,Qin, Sum;
+module multiplier #(parameter n = 8, freq = 3330000)
+				  (input logic startPB, input logic [1:0] func,
+				   input logic oe, output logic ready, inout [n - 1:0] data);
 
 //// Internal Oscillator 3.33MHz
 	defparam OSCH_inst.NOM_FREQ = "3.33";
-	OSCH OSCH_inst
-		( 
+	OSCH OSCH_inst ( 
 		.STDBY(1'b0), 		// 0=Enabled, 1=Disabled also Disabled with Bandgap=OFF
 		.OSC(osc_clk),
 		.SEDSTDBY()     		// this signal is not required if not using SED
-		);
+	);
+	//counter #(.n(24)) c(.*); // produces slow clock
+	assign clock = osc_clk;
 
-assign M = 4'b0101; // multiplicand - hardwired
-assign Qin = 4'b0111; // multiplier - hardwired
+//// Debounce
+	logic start;
+	debounce #(.n(freq / 1000)) d0(.clk(osc_clk), .in(startPB), .out(start));
 
-counter c(.*); // produces slow clock
-adder A(.A(AQ[7:4]), .M(M), .C(C), .Sum(Sum));
-register R(.*);
-sequencer S(.start(start), .clock(clock), .reset(reset), .Q0(AQ[0]),
- .add_shift(add_shift), .shift(shift), .ready(ready));
-  
+//// Blocks
+	logic C, reset, shift, add_shift;
+	logic [n - 1:0] Sum, M, Qin;
+	logic [n * 2 - 1:0] AQ;
+
+	adder #(.n(n)) A(.A(AQ[n * 2 - 1:n]), .*);
+	register #(.n(n)) R(.*);
+	sequencer #(.n(n)) S(.Q0(AQ[0]), .*);
+/*	combmultiplier #(.n(n)) c0 (.A(M), .B(Qin), .Q(AQ));
+	assign ready = 'b1;*/
+
+//// Port
+	assign data = oe ? (func == 2'b10 ? AQ[n - 1:0] : (func == 2'b11 ? AQ[n * 2 - 1:n] : 'bz)) : 'bz;
+
+	always_ff @(posedge osc_clk)
+		case (func)
+		2'b00:
+			M <= data;
+		2'b01:
+			Qin <= data;
+		endcase
+
 endmodule
