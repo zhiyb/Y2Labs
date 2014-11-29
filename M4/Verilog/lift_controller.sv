efc532853d0921bfc032741e825119ac2fc8cd05
zhiyb <yz39g13@soton.ac.uk>
2014-11-29 00:48:15 +0000

    M4 v3 20141112 Delayed at floors

--- M4/Verilog/lift_controller.sv
+++ M4/Verilog/lift_controller.sv
@@ -6,7 +6,7 @@
 // Description: lift controller
 //            :
 // Limitations: None
-//            : 
+//            :
 // System     : SystemVerilog IEEE 1800-2005
 //            :
 // Author     : Mark Zwolinski
@@ -15,58 +15,72 @@
 //            : Southampton SO17 1BJ, UK
 //            : mz@ecs.soton.ac.uk
 //
-// Revision   : Version 2.0 23/11/09 - modified by tjk for Altera DE0 Educational Board 
+// Revision   : Version 2.0 23/11/09 - modified by tjk for Altera DE0 Educational Board
 /////////////////////////////////////////////////////////////////////
 
 timeunit 1ns;
 timeprecision 100ps;
 
-module lift_controller (input logic clock, n_reset, top, middle_plus, middle_minus, bottom, call0, call1, call2, 
+module lift_controller (input logic clock, n_reset, top, middle_plus, middle_minus, bottom, call0, call1, call2,
   output logic direction, enable, indicator0, indicator1, indicator2,
   output logic led[9:0] // LEDs are used to display information about lift controller's behaviour
- );
+);
 
-  // SystemVerilog enumerated type for state machine states
-  enum {to0, floor0, floor1, floor2, downto1, upto1, to2} state;
-  
-  logic dir;
-  logic call[3];
+// SystemVerilog enumerated type for state machine states
+enum logic [2:0] {floor0, floor1, floor2, to0, to1, to2} state;
 
-always_ff @( posedge clock,
-            negedge n_reset) // always_ff means that this block is synthesised to flip-flops
-  begin: seq                 // note that this block also contains next state logic
-    if (~n_reset)
-    begin
-      state <= to0; //use non-blocking assignments for registered signals
-      call[0] <= 0;
-      call[1] <= 0;
-      call[2] <= 0;
-      dir <= 0;
-    end
-    else
-    begin
-      if (state == floor0)
-        call[0] <= 0;
-      else if (!call0)
-        call[0] <= !call0;
-      if (state == floor1)
-        call[1] <= 0;
-      else if (!call1)
-        call[1] <= !call1;
-      if (state == floor2)
-        call[2] <= 0;
-      else  if (!call2)
- 	      call[2] <= !call2;
+logic dir;
+logic call[3];
 
-      case (state)
-      floor0:
-        if (call[1] || call[2])
-        begin
-     	    state <= upto1;
-     	    dir <= 1;
-   	    end
+logic clear, timeout;
+lift_timer #(.t(50000000)) t0 (.*);
 
-      floor1: 
+// Call handling
+always_ff @(posedge clock, negedge n_reset)
+  if (~n_reset)
+  begin
+    call[0] <= 1'b0;
+    call[1] <= 1'b0;
+    call[2] <= 1'b0;
+  end
+  else
+  begin
+    if (state == floor0)
+      call[0] <= 1'b0;
+    else if (!call0)
+      call[0] <= 1'b1;
+    if (state == floor1)
+      call[1] <= 1'b0;
+    else if (!call1)
+      call[1] <= 1'b1;
+    if (state == floor2)
+      call[2] <= 1'b0;
+    else if (!call2)
+      call[2] <= 1'b1;
+  end
+
+// State
+// always_ff means that this block is synthesised to flip-flops
+always_ff @(posedge clock, negedge n_reset)
+begin: seq
+  if (~n_reset)
+  begin
+    state <= to0; //use non-blocking assignments for registered signals
+    dir <= 0;
+  end
+  else
+  begin
+    case (state)
+    floor0:
+      if (timeout && (call[1] || call[2]))
+      begin
+        state <= to1;
+        dir <= 1;
+      end
+
+    floor1:
+      if (timeout)
+      begin
         if (dir == 0 && call[0])
           state <= to0;
         else if (dir == 1 && call[2])
@@ -81,105 +95,82 @@ always_ff @( posedge clock,
           state <= to2;
           dir <= 1;
         end
+      end
 
-      floor2:
-        if (call[0] || call[1])
-        begin
-          state <= downto1;
-          dir <= 0;
-        end
+    floor2:
+      if (timeout && (call[0] || call[1]))
+      begin
+        state <= to1;
+        dir <= 0;
+      end
 
-      upto1: 
-        if (!middle_minus && !middle_plus)
-        begin
-          if (call[1])
-            state <= floor1;
-          else
-            state <= to2;
-        end
-	  
-      downto1:
-        if (!middle_minus && !middle_plus)
-        begin
-          if (call[1])
-            state <= floor1;
-          else
-            state <= to0;
-        end
+    to0:
+      if (!bottom)
+        state <= floor0;
 
-      to0:
-        if (!bottom)
-          state <= floor0;
+    to1:
+      if (!middle_minus && !middle_plus)
+      begin
+        if (call[1])
+          state <= floor1;
+        else if (dir)
+          state <= to2;
+        else
+          state <= to0;
+      end
 
-      to2:
-        if (!top)
-          state <= floor2;
+    to2:
+      if (!top)
+        state <= floor2;
 
-      default:
-        state <= to0;
-
-      endcase
-    end
-  end
-
-always_comb	   // always_comb - combinational logic for output signals; implicit sensitivity list
-  begin: com
-    // use blocking assignments for combinatorial signals
-    direction = '0; // 0 is down 
-    indicator0 = '1; // active low
-    indicator1 = '1; // active low
-    indicator2 = '1; // active low  
-    enable = '1;
-	
-	// leds are used to display information about the controller's operation
-	led[0] = (state == floor0);
-	led[1] = (state == floor1);
-	led[2] = (state == floor2);
-	led[3] = (state == to0);  // response to call0
-	led[4] = (state == downto1);  // response to call1 if on floor2
-	led[5] = (state == upto1);  // response to call1 if on floor0
-	led[6] = (state == to2);  // response to call2
-	led[7] =  top | middle_plus; // LEDs 7 and 8 show where the lift is:
-  led[8] =  top | middle_minus; // 00-bottom, 01-mid-, 10-mid+, 11-top
-	led[9] = ~n_reset; // this led lights up if reset is active
- 
-    indicator0 = !call[0];
-    indicator1 = !call[1];
-    indicator2 = !call[2];
- 
-    case (state)
-       upto1: 
-        if (middle_minus || middle_plus)
-          begin
-            enable = '0;
-	          direction = '1;
-	          //indicator1 = '0;
-          end
-      downto1: 
-        if (middle_minus || middle_plus)
-          begin
-            enable = '0;
-	        direction = '0;
-	        //indicator1 = '0;
-          end
-      to0: 
-        if (bottom)
-          begin
-            enable = '0;
-	        direction = '0;
-	        //indicator0 = '0;
-          end
-      to2: 
-        if (top)
-          begin
-            enable = '0;
-	        direction = '1;
-	        //indicator2 = '0;
-          end
-      default: ;
+    default:
+      state <= to0;
 
     endcase
   end
+end
 
+// Timer handling
+always_comb
+  if (state == floor0 || state == floor1 || state == floor2)
+    clear = 1'b0;
+  else
+    clear = 1'b1;
+
+// Output signals
+always_comb   // always_comb - combinational logic for output signals; implicit sensitivity list
+begin: com
+  // use blocking assignments for combinatorial signals
+  indicator0 = !call[0];
+  indicator1 = !call[1];
+  indicator2 = !call[2];
+  direction = dir;
+  enable = '1;
+
+  // leds are used to display information about the controller's operation
+  led[0] = (state == floor0);
+  led[1] = (state == floor1);
+  led[2] = (state == floor2);
+  led[3] = (state == to0);  // state to0
+  led[4] = (state == to1);  // state to1
+  led[5] = (state == to2);  // state to2
+  led[6] = 1'b0;
+  led[7] = top | middle_plus; // LEDs 7 and 8 show where the lift is:
+  led[8] = top | middle_minus; // 00-bottom, 01-mid-, 10-mid+, 11-top
+  led[9] = ~n_reset; // this led lights up if reset is active
+
+  case (state)
+  to0:
+    if (bottom)
+      enable = '0;
+  to1:
+    if (middle_minus || middle_plus)
+      enable = '0;
+  to2:
+    if (top)
+      enable = '0;
+  default: ;
+  endcase
+end
 
 endmodule
